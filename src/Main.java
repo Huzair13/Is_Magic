7f80e0b3c17d0bd9cd9b4134e2678cf8be1cb25f
Huzair13 <ahamedhuzair13@gmail.com>
2023-07-21 07:43:22 +0530

    initial

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter the number !!! ");
+        int number=scanner.nextInt();
+
+        int isMagic=findIsMagic(number);
+        System.out.println(isMagic);
+    }
+
+    private static int findIsMagic(int number) {
+        if(number==1){
+            return 1;
+        }
+        else if(number>1 && number<10) {
+            return 0;
+        }
+        else{
+            return(findIsMagic(digitSum(number)));
+        }
+    }
+
+    private static int digitSum(int number){
+        if(number==0){
+            return 0;
+        }
+        return number%10 + digitSum(number/10);
+    }
+}
